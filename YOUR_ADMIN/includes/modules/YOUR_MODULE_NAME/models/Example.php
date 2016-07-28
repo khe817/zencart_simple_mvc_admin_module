fede8bf1265f6c25e10f4ef0d38076b136741697
Ngan Bui <khe817@gmail.com>
2016-07-28 16:33:01 +0000

    Fix query in model class.

--- YOUR_ADMIN/includes/modules/YOUR_MODULE_NAME/models/Example.php
+++ YOUR_ADMIN/includes/modules/YOUR_MODULE_NAME/models/Example.php
@@ -16,7 +16,7 @@ class ExampleModel {
 	public function get_current_admin_name () {
 		global $db;
 
-		$current_admin = $db->Execute('SELECT * FROM ' . TABLE_ADMIN . ' WHERE admin_id = "' . $_SESSION['admin_id'] . '"');
+		$current_admin = $db->Execute('SELECT * FROM ' . TABLE_ADMIN . ' WHERE admin_id = "' . (int)$_SESSION['admin_id'] . '"');
 
 		if ( $current_admin->RecordCount() > 0 ) {
 			return $current_admin->fields['admin_name'];
