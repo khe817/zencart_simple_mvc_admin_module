708d4d4df89e5013e8c56165797d9e650177366d
khe817 <khe817@gmail.com>
2016-10-04 09:37:34 -0400

    Better sql querry preparation in example models

--- YOUR_ADMIN/includes/modules/YOUR_MODULE_NAME/models/Example.php
+++ YOUR_ADMIN/includes/modules/YOUR_MODULE_NAME/models/Example.php
@@ -15,7 +15,9 @@ class ExampleModel {
 	public function get_current_admin_name () {
 		global $db;
 
-		$current_admin = $db->Execute('SELECT * FROM ' . TABLE_ADMIN . ' WHERE admin_id = "' . (int)$_SESSION['admin_id'] . '"');
+		$current_admin_sql = 'SELECT admin_name FROM ' . TABLE_ADMIN . ' WHERE admin_id = :admin_id';
+		$current_admin_sql = $db->bindVars($current_admin_sql, ':admin_id', (int)$_SESSION['admin_id'], 'integer');
+		$current_admin = $db->Execute($current_admin_sql);
 
 		if ( $current_admin->RecordCount() > 0 ) {
 			return $current_admin->fields['admin_name'];
--- YOUR_ADMIN/includes/modules/YOUR_MODULE_NAME/models/example.php
+++ YOUR_ADMIN/includes/modules/YOUR_MODULE_NAME/models/example.php
@@ -10,7 +10,9 @@ if ( !defined('IS_ADMIN_FLAG') ) {
 function get_current_admin_name () {
 	global $db;
 
-	$current_admin = $db->Execute('SELECT * FROM ' . TABLE_ADMIN . ' WHERE admin_id = "' . (int)$_SESSION['admin_id'] . '"');
+	$current_admin_sql = 'SELECT admin_name FROM ' . TABLE_ADMIN . ' WHERE admin_id = :admin_id';
+	$current_admin_sql = $db->bindVars($current_admin_sql, ':admin_id', (int)$_SESSION['admin_id'], 'integer');
+	$current_admin = $db->Execute($current_admin_sql);
 
 	if ( $current_admin->RecordCount() > 0 ) {
 		return $current_admin->fields['admin_name'];
